9596c7abdb962c862058cce84d1d98145d982f82
osawa-koki <github@osawa.dev>
2022-12-31 17:37:33 +0900

    テストコード消した。

--- src/test/java/com/example/demo/WebToDoApplicationTests.java
+++ src/test/java/com/example/demo/WebToDoApplicationTests.java
@@ -1,14 +1,14 @@
-package com.example.demo;
-
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class WebToDoApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
-}
+//package com.example.demo;
+//
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//public class WebToDoApplicationTests {
+//
+//	@Test
+//	public void contextLoads() {
+//	}
+//
+//}
